88dba6ea477199aa50655e788ca614e6a9f94a7c
reyes630 <reyesagudeloesteban9@gmail.com>
2025-08-22 11:51:04 -0500

    Fix name model user in file rol

--- models/rols.js
+++ models/rols.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      rols.hasMany(models.User, { foreignKey: 'rolId' });
+      rols.hasMany(models.user, { foreignKey: 'rolId' });
     }
   }
   rols.init({
